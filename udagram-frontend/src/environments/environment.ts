dff2026c195108a7b4e39fca25a9add026230c27
Clifford Usidamen <cliffordusidamen@gmail.com>
2022-07-17 00:53:08 +0100

    Change all the deployment yaml files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6d463bbe7cc94fd0a7c48493bc4f92c-391704583.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
